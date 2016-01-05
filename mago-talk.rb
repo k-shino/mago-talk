79733d7829b84de19733f0768300a726b8acbcf3
Ubuntu <ubuntu@ip-172-31-12-216.ap-northeast-1.compute.internal>
2016-01-05 14:07:35 +0000

    fix

--- mago-talk.rb
+++ mago-talk.rb
@@ -15,6 +15,11 @@ end
 get '/' do
 	client = Docomoru::Client.new(api_key: "376f5479654d35514574463155363475396c4d6661694f6866576e556c487a5a7669356b394a795068472f")
 	response = client.create_dialogue("こんにちは", {"age" => "2","t" => "30"})
+
+@ip = Socket.getifaddrs.select{|x|
+  x.name == "eth0" and x.addr.ipv4?
+}.first.addr.ip_address
+
     @title = container = `hostname` || 'unknown'
 	@body = response.body
 	erb :index
